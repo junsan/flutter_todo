52231370287d3e97a31a6530c8b47843ad86dd9b
Jun Santos <xjunsantos@gmail.com>
2024-03-23 20:13:53 +0800

    upload files

---
+++ lib/screens/tasks_screen.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class TasksScreen extends StatelessWidget {
+  const TasksScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlueAccent,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: EdgeInsets.only(top: 40, left: 20, bottom: 20),
+            child: const Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CircleAvatar(
+                    backgroundColor: Colors.white,
+                    child: Icon(Icons.list, color: Colors.lightBlueAccent,)
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text('Todoey',
+                  style: TextStyle(
+                    fontSize: 45,
+                    fontWeight: FontWeight.w700,
+                    color: Colors.white
+                  )
+                ),
+                Text('12 Tasks',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18
+                  ),
+                )
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(topLeft: Radius.circular(30), topRight: Radius.circular(30))
+              ),
+            ),
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.lightBlueAccent,
+        shape: CircleBorder(),
+        child: Icon(Icons.add, color: Colors.white,),
+      ),
+    );
+  }
+}
